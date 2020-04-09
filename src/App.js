30f3b9c4d9dbf224f4fbd0d24bc7fc40282be61e
Blazej <kucmeno12@o2.pl>
2020-04-09 09:43:41 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Discover React
+          Discover and learn React
         </a>
       </header>
     </div>
